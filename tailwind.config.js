2e3f76a476157e37e5ae2a869824d7953f3b8a63
Juwan <jhollingsworth5@student.clayton.edu>
2023-07-12 12:34:46 -0400

    add custom media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xsm: "350px",
+    },
     extend: {
       fontFamily: {
         burtons: "burtons",

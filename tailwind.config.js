a160bb4d85094fe3e65fd3dde419e0de08d79e9d
Juwan <jhollingsworth5@student.clayton.edu>
2023-07-12 12:59:08 -0400

    define additional queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   theme: {
     screens: {
       xsm: "350px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     extend: {
       fontFamily: {
